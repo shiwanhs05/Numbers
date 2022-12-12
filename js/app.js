a33f2357e6f1c9b97b13bb3b45355f80778e3e17
Shiwanhs05 <shiwansh452@gmail.com>
2022-12-12 22:31:39 +0530

    complete project

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const items = [...document.querySelectorAll('.number')];
+
+
+const updateCount = (el)=>{
+  const value = parseInt(el.dataset.value);
+  const increment = Math.ceil(value/1000);
+  // const increment = 1;
+  let initialValue = 0;
+  const increaseCount = setInterval(()=>{
+    initialValue += increment;
+    el.textContent = `${initialValue}+`;
+    if(initialValue > value){
+      el.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+  }, 1);
+  // intervalID = unique Id return by setInterval function that can be used to clear interval
+  console.log(increaseCount);
+};
+items.forEach((item) => {
+  updateCount(item);
+});
